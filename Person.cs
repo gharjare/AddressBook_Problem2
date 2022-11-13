20aef34800f9485fb467b8011a71dcee765e9a22
gharjare <gharjarechetan@gmail.com>
2022-11-13 15:10:12 +0530

    added text

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook2
+{
+    public class Person
+    {
+        public string Name;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+
+        public Person(string name, string address, string city, string state, string zip, string phonenumber, string email)
+        {
+            Name = name;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phonenumber;
+            Email = email;
+
+        }
+    }
+}
