cfd8ae578891402e7f36dc2eb5979889f4c7966a
gharjare <gharjarechetan@gmail.com>
2022-11-13 15:11:27 +0530

    Added dictionary

---
+++ Dictionary.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBook2
+{
+    internal class Dictionary<T1, T2, T3>
+    {
+        internal void Add(string v1, string v2, string v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
